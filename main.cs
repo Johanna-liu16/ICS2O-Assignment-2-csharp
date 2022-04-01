5b83c30672cfa17a9a08f45f2374b9688bfa515e
Johanna-liu16 <99354992+Johanna-liu16@users.noreply.github.com>
2022-04-01 00:55:54 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,28 @@
+// Created by: Johanna Liu
+// Created on: Mar 2022
+//
+// This program does basic math
+
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        // This function states user's address input.
+        int bases;
+        int height;
+
+        Console.WriteLine("This program will calculate the volume of a pyramid.");
+        Console.WriteLine("");
+        Console.Write("Enter base: ");
+        bases = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter height: ");
+        height = Convert.ToInt32(Console.ReadLine());
+      
+        Console.WriteLine("");
+        Console.WriteLine("The area is: " + (bases * height).ToString("0.00") + " cm².");
+
+        Console.WriteLine("\nDone.");
+    }
+}
